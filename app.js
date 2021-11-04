3de6c008b80347fc38d0e4685df6af984cb2e30f
Shylko Nastassia <wcganastacia@yandex.ru>
2021-11-04 14:28:55 +0300

    add ejs

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require('http')
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-const server =  http.createServer()
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-server.listen(3000)
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'))
+})
+
+app.listen(3000)
